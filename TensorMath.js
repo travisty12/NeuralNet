c5ea229c11dcabc2fbeb2efbfa36d544e07a0488
Travis Scott <travisty12@gmail.com>
2020-12-24 15:47:00 -0800

    clean up documentation, fill out randn function

--- TensorMath.js
+++ TensorMath.js
@@ -1,12 +1,29 @@
 class TensorMath {
-  static normalDistribution() { // gaussian curve using Box-Muller transform (https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform)
+
+  // gaussian curve using Box-Muller transform (https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform)
+  static normalDistribution() { 
     let [u, v] = [Math.random(), Math.random()];
-    while (!(u && v)) [u, v] = [Math.random(), Math.random()]; // prevents the slim chance of either of them picking 0
+    // prevents the slim chance of either of them picking 0
+    while (!(u && v)) [u, v] = [Math.random(), Math.random()]; 
     return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
   }
 
-  static randn(x = 0, y = 0) { // with no inputs, returns a random normal distribution float. with 1 input, an array of length x, of normal distribution floats. with 2 inputs, it retuns a 2-D x by y array of normal distribution floats. built this way to mirror NumPy's randn function.
-    const output = []; // Double input -- array of arrays.
+  // With no inputs, returns a random normal distribution float. With 1 input, an array of length x, of normal distribution floats. With 2 inputs, it retuns a 2-D x by y array of normal distribution floats. Built this way to mirror NumPy's randn function.
+  static randn(x = 0, y = 0) { 
+    // Base case: just return normal distribution
+    if (!(x || y)) return TensorMath.normalDistribution();
+    
+    // Single input -- array of length x of base case returned
+    if (!y) { 
+      const output = []; 
+      for (let i = 0; i < x; i++) { // can't use Array.fill, because that will force every index to have the same random value
+        output.push(TensorMath.randn());
+      }
+      return output;
+    }
+
+    // Double input -- array of arrays.
+    const output = []; 
     for (let i = 0; i < x; i++) { // can't use Array.fill, because that will force every index to have the same random value
       output.push(TensorMath.randn(y));
     }
