83c018049704a304fb4aadb336a8d42b78a550d1
Travis Scott <travisty12@gmail.com>
2020-12-24 15:36:04 -0800

    add explanations for bias and weight size

--- NeuralNet.js
+++ NeuralNet.js
@@ -2,6 +2,16 @@ class Network {
   constructor(sizes, name) { // takes in an array whose indices represent the number of neurons in each layer, e.g. [10, 16, 4] could represent a network with 10 inputs, a hidden layer of 16 neurons, and an output layer of 4 neurons
     this.name = name,
     this.num_layers = sizes.length,
-    this.sizes = sizes
+    this.sizes = sizes,
+    this.biases = this.generateBiases(), // randomized biases for neurons of each layer EXCEPT the inputs on a normal distribution (gaussian curve). So for the above example, [new Array(16).fill(normal distribution), new Array(4).fill(normal distribution)]
+    this.weights = this.generateWeights()// randomized weights connecting neurons of each layer on a normal distribution (gaussian curve). So for the above example, [[new Array(16).fill(new Array(10).fill(normal distrubtion))], new Array(4).fill(new Array(16).fill(normal distribution))]
+  }
+
+  generateBiases() {
+    return 0;
+  }
+
+  generateWeights() {
+    return 0;
   }
 }
---
+++ TensorMath.js
@@ -0,0 +1,5 @@
+class TensorMath {
+  static normalDistribution() { // gaussian curve using Box-Muller transform (https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform)
+
+  }
+}
