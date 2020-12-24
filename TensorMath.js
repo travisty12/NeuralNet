a31316627c9258131bef020c36ac65b6a699bd9b
Travis Scott <travisty12@gmail.com>
2020-12-24 15:37:58 -0800

    add normal distribution function

--- TensorMath.js
+++ TensorMath.js
@@ -1,5 +1,7 @@
 class TensorMath {
   static normalDistribution() { // gaussian curve using Box-Muller transform (https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform)
-
+    let [u, v] = [Math.random(), Math.random()];
+    while (!(u && v)) [u, v] = [Math.random(), Math.random()]; // prevents the slim chance of either of them picking 0
+    return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
   }
 }
